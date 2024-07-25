738031f821019610059e526ee1f871cba25268d7
kafierence <vannam34760@gmail.com>
2024-07-25 21:34:06 +0700

    [update]

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,8 @@ import ProviderApp from "@/provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Starkflip ",
+  title: "Starkflip Game | StarkArcade Hub",
+  metadataBase: new URL("https://starkflip.starkarcade.com/"),
   description:
     "Starknet Arcade Hub is a tribute to our NFT community and one of the largest mini-games hub for the Starknet Degens. The first product with upcominng Beta will be a CoinFlip game, allowing folks to place bets and multiply their $ETH holdings on Starknet",
   icons: {
@@ -15,14 +16,47 @@ export const metadata: Metadata = {
     apple: Favicon.src,
     other: { rel: "apple-touch-icon-precomposed", url: Favicon.src },
   },
-  keywords: [
-    "StarkArcade",
-    "What is StarkArcade",
-    "Starknet Hub",
-    "Starknet Arcade Hub",
-    "Starknet Arcade",
-  ],
+
+  keywords: ["Starkflip", "What is Starkflip", "StarkArcade Hub"],
+  openGraph: {
+    title: "Starkflip | StarkArcade Hub",
+    description:
+      "Starknet Arcade Hub is a tribute to our NFT community and one of the largest mini-games hub for the Starknet Degens. The first product with upcominng Beta will be a CoinFlip game, allowing folks to place bets and multiply their $ETH holdings on Starknet",
+    images: [
+      {
+        url: "https://starkflip.starkarcade.com/assets/banner/banner.png",
+        width: 1200,
+        height: 600,
+        type: "image/png",
+      },
+    ],
+    locale: "en_US",
+    url: "https://starkflip.starkarcade.com",
+    type: "website",
+    emails: "karasbuilder@gmail.com",
+    siteName: "Starkflip Game",
+  },
+  twitter: {
+    title: "Starkflip Game | StarkArcade Hub",
+    description:
+      "Starknet Arcade Hub is a tribute to our NFT community and one of the largest mini-games hub for the Starknet Degens. The first product with upcominng Beta will be a CoinFlip game, allowing folks to place bets and multiply their $ETH holdings on Starknet",
+    images: {
+      url: "https://starkflip.starkarcade.com/assets/banner/banner.png",
+      alt: "Tetris Banner",
+    },
+    site: "https://starkflip.starkarcade.com",
+    card: "player",
+    creator: "@karas_builder",
+    players: {
+      playerUrl: "https://starkflip.starkarcade.com",
+      streamUrl: "https://starkflip.starkarcade.com",
+      width: 600,
+      height: 600,
+    },
+  },
+  category: "technology",
 };
+
 export default function RootLayout({
   children,
 }: Readonly<{
